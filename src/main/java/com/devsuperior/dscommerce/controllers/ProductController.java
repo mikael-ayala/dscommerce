cf3f58ae1b57247498f8d933256aa3f14e59a6d9
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-11-26 23:02:32 -0300

    GET Product by Id

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
-    @GetMapping
-    public String olaMundo() {
-        return "Olá Mundo";
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return productService.findById(id);
     }
 }
---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public record ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+    public ProductDTO(Product product) {
+        this(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getImgUrl());
+    }
+}
