53d526e27ecdaa933efe14cbf803ddd58c1034b8
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-11-26 22:01:19 -0300

    add ProductController

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -92,7 +92,7 @@ public class Product {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) {;
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
